52e0e3c550be17a41e6ff69c73f981e57727020e
ryiephry <ephraimpt2@gmail.com>
2023-08-06 23:43:50 -0400

    2.5

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
 import axios from 'axios';
-
 const formEl = document.querySelector('.search-form');
 const gallery = document.querySelector(".gallery");
-const loadmoreEl = document.querySelector(".load-more")
-console.log(loadmoreEl)
-// Initialize current page number
+const loadMoreEl = document.querySelector(".load-more");
+const inputEl = document.querySelector("[name=searchQuery]"); 
+const input = ''
 let page = 1;
-
+    
 // Add an event listener to the form to handle form submissions
 formEl.addEventListener('submit', handleFormSubmit);
 
 // Function to handle form submissions
 async function handleFormSubmit(e) {
   e.preventDefault(); // Prevent the default form submission behavior
+  let userInput = " ";
 
+  console.log(inputEl.value);
   // Get the user input from the first input element in the form and remove any leading/trailing white spaces
-  const userInput = e.target[0].value.trim();
+  if (e.target.className === "load-more-unhidden") {
+    // userInput = e.target[0].value.trim();
+
+    userInput = e.target.value.trim();
+  } else {
+    userInput = inputEl.value.trim();
+    // currentUserInput = userInput;
+  }
 
   // Fetch data from the API using the user input and the current page number
   const data = await fetchData(userInput, page);
-
   // Log the hits array from the fetched data (each hit represents an image)
   console.log(data.hits);
-
-  // Check if there are no images found based on the total count from the API response
-  if (data.total === 0) {
-    // Display an alert to the user if no images are found
+  if (data.total === 0) { // chekc for images
     alert('Sorry, no images found. Please try again.');
   } else {
-    createMarkup(data.hits);  // a number 
+    createMarkup(data.hits);  // a number
   }
-
   // Reset the input value to an empty string after the form is submitted
-  e.target[0].value = '';
+  if (e.target.className != "load-more-unhidden") {
+    // input = inputEl.value ; console.log(input);
+    e.target[0].value = '';
+    page += 1;
+  } 
 }
 
 // Function to fetch data from the Pixabay API based on user input and page number
@@ -42,24 +49,21 @@ async function fetchData(userInput, page) {
     const response = await axios.get(
       `https://pixabay.com/api/?key=38245803-17b2e774beea3b422758604fe&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
     );
-
     // Return the data (hits array and other information) from the API response
+    console.log(page)
     return response.data;
   } catch (error) {
     // If an error occurs during the API call, log the error message and throw the error
     console.error('Error fetching data:', error);
-    throw error; // You can handle the error further if needed
+    throw error; 
   }
 }
 
-// Function to create the necessary markup and display the images on the page
+// Function to create markup
 function createMarkup(hits) {
-
     for (let i = 0; i < hits.length; i++){
-
        let divEl = document.createElement("div")
         divEl.className = "photo-card";
-
        divEl.innerHTML =   `<img src="${hits[i].webformatURL}" alt="${hits[i].tags}" loading="lazy" />
             <div class="info">
                 <p class="info-item">
@@ -67,12 +71,10 @@ function createMarkup(hits) {
                 <br>
                 ${hits[i].likes}
                 </p>
-
                 <p class="info-item">
                 <b>Views</b>
                  ${hits[i].views}
                 </p>
-               
                 <p class="info-item">
                 <b>Comments</b>
                 <br>
@@ -84,14 +86,12 @@ function createMarkup(hits) {
                 ${hits[i].downloads}
                 </p>
             </div>`;
-        
         gallery.append(divEl)
-    }
-   
-
-    console.log(hits.length)
+  };
+  loadMoreEl.classList.add("load-more-unhidden");
+  loadMoreEl.classList.remove("load-more")
+  // console.log(hits.length)
 }
 
-
-
+loadMoreEl.addEventListener("click", handleFormSubmit);
 
