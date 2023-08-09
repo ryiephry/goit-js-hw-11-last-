e23ee9ebd2f64e4943c20c3083bd3227f2e4bdf2
ryiephry <ephraimpt2@gmail.com>
2023-08-08 20:01:40 -0400

    4.3

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function handleFormSubmit(e) {
   
 
   // checks if the current value is equal to previous value ,  and  if the button thats being pressed isnt the load more button then it executes 
-  if (inputEl.value !== userInput && e.target.className !== 'load-more-unhidden'   ) { 
+  if (inputEl.value !== userInput && e.target.className !== 'load-more-unhidden') {
     gallery.innerHTML = ""
     page = 1
 
@@ -43,18 +43,21 @@ async function handleFormSubmit(e) {
     console.log(data.totalHits)
     createMarkup(data.hits);
   }
+
+
   let totalImages = page * 40;
 
-      // if the page times the amount of images => totalHits , alerts screen 
+  // if the page times the amount of images => totalHits , alerts screen 
   let dataEl = data.hits;
 
-   if (data.hits < 40) { 
-     console.log("less hits")
+
+  if (data.hits < 40) {
+    console.log("less hits")
      
     //  loadMoreEl.classList.add("load-more");
-     loadMoreEl.classList.remove("load-more-unhidden");
-     loadMoreEl.classList.add("load-more");
-   }
+    loadMoreEl.classList.remove("load-more-unhidden");
+    loadMoreEl.classList.add("load-more");
+  }
   
   
   if (totalImages === data.totalHits && totalImages > data.totalHits) {
@@ -65,41 +68,41 @@ async function handleFormSubmit(e) {
 
     loadMoreEl.classList.add("load-more");
    
-  } else { 
-    loadMoreEl.classList.remove("load-more")
-  }
+    // } else { 
+    //   loadMoreEl.classList.remove("load-more")
+    // }
    
 
-  // Reset the input value to an empty string after the form is submitted
-  if (e.target.className !== 'load-more-unhidden') {
-    inputEl.value = '';
+    // Reset the input value to an empty string after the form is submitted
+    if (e.target.className !== 'load-more-unhidden') {
+      inputEl.value = '';
+    }
+    page += 1;
   }
-  page += 1;
-}
-// Rest of the code remains unchanged... // ..
-// Function to fetch data from the Pixabay API based on user input and page number
-async function fetchData(userInput, page) {
-  try {
-    // Make an HTTP GET request using Axios to the Pixabay API
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=38245803-17b2e774beea3b422758604fe&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-    );
-    // Return the data (hits array and other information) from the API response
-    console.log(page);
-    return response.data;
-  } catch (error) {
-    // If an error occurs during the API call, log the error message and throw the error
-    console.error('Error fetching data:', error);
-    throw error;
+  // Rest of the code remains unchanged... // ..
+  // Function to fetch data from the Pixabay API based on user input and page number
+  async function fetchData(userInput, page) {
+    try {
+      // Make an HTTP GET request using Axios to the Pixabay API
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=38245803-17b2e774beea3b422758604fe&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+      );
+      // Return the data (hits array and other information) from the API response
+      console.log(page);
+      return response.data;
+    } catch (error) {
+      // If an error occurs during the API call, log the error message and throw the error
+      console.error('Error fetching data:', error);
+      throw error;
+    }
   }
-}
 
-// Function to create markup
-function createMarkup(hits) {
-  for (let i = 0; i < hits.length; i++) {
-    let divEl = document.createElement('div');
-    divEl.className = 'photo-card';
-    divEl.innerHTML = `<img src="${hits[i].webformatURL}" alt="${hits[i].tags}" loading="lazy" />
+  // Function to create markup
+  function createMarkup(hits) {
+    for (let i = 0; i < hits.length; i++) {
+      let divEl = document.createElement('div');
+      divEl.className = 'photo-card';
+      divEl.innerHTML = `<img src="${hits[i].webformatURL}" alt="${hits[i].tags}" loading="lazy" />
             <div class="info">
                 <p class="info-item">
                 <b>Likes</b>
@@ -121,14 +124,14 @@ function createMarkup(hits) {
                 ${hits[i].downloads}
                 </p>
             </div>`;
-    gallery.append(divEl);
-  }
+      gallery.append(divEl);
+    }
   
-  if (dataEl > 40) {
-    loadMoreEl.classList.remove('load-more');
-  }
-  loadMoreEl.classList.add('load-more-unhidden');
+    if (dataEl > 40) {
+      loadMoreEl.classList.remove('load-more');
+    }
+    loadMoreEl.classList.add('load-more-unhidden');
  
+  }
 }
-
 loadMoreEl.addEventListener("click", handleFormSubmit);
